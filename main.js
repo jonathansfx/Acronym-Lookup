03f5d12c246bc77b49abc18ad30d693e2d3cf28e
jonathansfx <jonathanluo22@gmail.com>
2022-11-30 14:36:31 -0700

    finish

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 //Acronym Lookup
 
 //HTML variables
-let abbEl = document.getElementById("abbmeaning");
+
 //Button Event Listener
 document.getElementById("btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
   //Input
-  let abb = document.getElementById("abb-in");
+  let abb = document.getElementById("output");
+  let x = document.getElementById("input").value.toLowerCase();
   //If statement
-  if (abb === "html") {
-    document.getElementById(abbMeaning) = "HyperText Markup Language";
+  if (x === "html") {
+    abb.innerHTML = "HyperText Markup Language";
+  } else if (x === "js") {
+    abb.innerHTML = "JavaScript";
+  } else if (x === "css") {
+    abb.innerHTML = "Cascading Style Sheets";
+  } else if (x === "pc") {
+    abb.innerHTML = "Personal Computer";
+  } else if (x === "asap") {
+    abb.innerHTML = "As Soon As Possible";
+  } else {
+    abb.innerHTML = "I don't know what that is";
   }
 }
